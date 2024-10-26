4b49d08230a252779e0e1ea4542703be014db38e
asgerami <amiraman467@gmail.com>
2024-10-26 20:45:52 +0300

    feat: created landing screen with Header and Hero components
    - Built responsive Header with navigation links, login, and register buttons
    - Developed Hero section
    - Combined Header and Hero into Home component for cohesive landing screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Header from "./_components/Header";
+import Hero from "./_components/Hero";
 
 export default function Home() {
   return (
     <div>
-      <h1>day 1</h1>
+      <Header />
+      <Hero />
     </div>
   );
 }
