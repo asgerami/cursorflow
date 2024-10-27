7dbb004aa3c30df382ccb22fd7644af34dd67053
asgerami <amiraman467@gmail.com>
2024-10-27 15:41:20 +0300

    auth: integrate Kinde login, logout, and dashboard access control

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+"use client";
+import { Button } from "@/components/ui/button";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs";
+import React from "react";
 
 function Dashboard() {
   return (
     <div>
       Dashboard
+      <Button>
+        <LogoutLink>Logout</LogoutLink>
+      </Button>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
