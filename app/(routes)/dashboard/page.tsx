a15ad12a788206d8e832f5d95161b1f32cc47bd7
asgerami <amiraman467@gmail.com>
2024-10-28 17:30:26 +0300

    Add user query and mutation functions
    - Implement `getUser` query to retrieve user by email
    - Implement `createUser` mutation to insert a new user with name, email, and image fields
    - Use Convex `v` for type validation and database query handling

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,12 +1,38 @@
 "use client";
+
 import { Button } from "@/components/ui/button";
-import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs";
-import React from "react";
+import { api } from "@/convex/_generated/api";
+import { LogoutLink, useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import { useMutation, useQuery } from "convex/react";
+import React, { useEffect } from "react";
 
 function Dashboard() {
+
+  const {user}:any = useKindeBrowserClient();
+  const getUser=useQuery(api.user.getUser, {email:user?.email});
+  
+  const createUser=useMutation(api.user.createUser);
+
+  useEffect(() => {
+    if(getUser) 
+      {
+        if(getUser==undefined)
+      {
+        createUser({
+          name: user.given_name,
+          email: user.email,
+          image: user.picture
+        }).then((resp)=>{
+          console.log(resp)
+        })
+      }
+    }
+
+  },[user])
+
+
   return (
     <div>
-      Dashboard
       <Button>
         <LogoutLink>Logout</LogoutLink>
       </Button>
