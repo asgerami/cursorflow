fe5999ea638463c789bde27e6e1efcc021104b4c
asgerami <amiraman467@gmail.com>
2024-10-31 20:42:37 +0300

    chore: add imports for required modules

--- app/(routes)/dashboard/layout.tsx
+++ app/(routes)/dashboard/layout.tsx
@@ -4,6 +4,7 @@ import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 import { useConvex } from "convex/react";
 import { useRouter } from "next/navigation";
 import React, { useEffect } from "react";
+import SideNav from "./_components/SideNav";
 
 function DashboardLayout({
   children,
@@ -27,7 +28,18 @@ function DashboardLayout({
     }
   };
 
-  return <div>{children}</div>;
+  return (
+    <div>
+      <div className="grid grid-cols-4">
+        <div>
+          <SideNav />
+        </div>
+        <div className="gird-cols-3">
+          {children}
+          </div>
+      </div>
+    </div>
+  );
 }
 
 export default DashboardLayout;
