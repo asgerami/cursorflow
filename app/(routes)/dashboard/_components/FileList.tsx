a32ce3f64cd14d9ecacecdff1eb9ccb20c6d5ebe
asgerami <amiraman467@gmail.com>
2024-11-06 12:14:52 +0300

    feat: add navigation to FileList rows and improve UX with cursor pointer

--- app/(routes)/dashboard/_components/FileList.tsx
+++ app/(routes)/dashboard/_components/FileList.tsx
@@ -12,6 +12,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { useRouter } from "next/navigation";
 
 export interface FILE {
   archive: boolean;
@@ -28,6 +29,7 @@ function FileList() {
   const { fileList_, setFileList_ } = useContext(FileListContext);
   const [fileList, setFileList] = useState<any>();
   const { user }: any = useKindeBrowserClient();
+  const router = useRouter();
 
   useEffect(() => {
     fileList_ && setFileList(fileList_);
@@ -58,7 +60,10 @@ function FileList() {
           <tbody className="divide-y divide-gray-200">
             {fileList &&
               fileList.map((file: FILE, index: number) => (
-                <tr className="odd:bg-gray-50">
+                <tr
+                  className="odd:bg-gray-50 cursor-pointer"
+                  onClick={() => router.push("/workspace/" + file._id)}
+                >
                   <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
                     {file.fileName}
                   </td>
